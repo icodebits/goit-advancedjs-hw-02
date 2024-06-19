8b484538b7b1ef50cea202ce2fcc19f81a3c89ff
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-06-19 18:23:21 +0300

    Add task 3.

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const delay = Number(event.target.delay.value);
+  const step = Number(event.target.step.value);
+  const amount = Number(event.target.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const currentDelay = delay + i * step;
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          title: 'Success',
+          message: `✅ Fulfilled promise ${position} in ${delay}ms`,
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Rejected promise ${position} in ${delay}ms`,
+        });
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
